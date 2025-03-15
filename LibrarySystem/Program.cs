2515b0b8c9d7a00f8aafd2723c00ea8dc68887d8
Omar Sayed <151388740+OmarSayed0@users.noreply.github.com>
2025-03-15 17:52:39 +0200

    Implement Home Screen Logic

--- LibrarySystem/Program.cs
+++ LibrarySystem/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to Library management system v1.0 ");
+            Console.WriteLine(
+                """
+                Please select type of user :
+                1.Librarian(L) 
+                2.Member(M)
+                """);
+
+            var userType = Console.ReadLine();
+            switch (userType)
+            {
+                case "L":
+                    Console.WriteLine("Welcome Librarian");
+                    break;
+                case "M":
+                    Console.WriteLine("Welcome Member");
+                    break;
+                default:
+                    Console.WriteLine("Invalid user type");
+                    break;
+            }
+
+
+
         }
     }
 }
